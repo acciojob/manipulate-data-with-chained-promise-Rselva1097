1ccb7e0f3edf402edf5658cc1a5146aed8e0192c
Rselva1097 <99159175+Rselva1097@users.noreply.github.com>
2024-05-28 07:21:35 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+const arr=[1,2,3,4];
+
+function getNumbers()=>{
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(arr);
+		},3000)
+	})
+}
+
+ function updateOutput(elementId, text) {
+            document.getElementById(elementId).textContent = text;
+        }
+
+function processNumbers()=>{
+	getNumbers().then((number)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const evenNumber=number.filter(a => a%2 == 0)
+				updateOutput("output",evenNumber.join(','))
+			},1000)
+		})
+	})
+	.then(evenNumbers => {
+                    // Multiply all even numbers by 2
+                    return new Promise((resolve) => {
+                        setTimeout(() => {
+                            const multipliedNumbers = evenNumbers.map(num => num * 2);
+                            updateOutput('output', multipliedNumbers.join(', '));
+                            resolve(multipliedNumbers);
+                        }, 2000);
+                    });
+                })
+                .catch(error => console.error(error));
+}
+
+processNumbers()
+
+
